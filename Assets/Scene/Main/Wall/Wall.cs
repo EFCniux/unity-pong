0a2943f56102f947f42600be3df237d0ba0848aa
Eduardo Ferreras Carracedo <efc.niux@gmail.com>
2021-01-09 21:31:01 +0100

    moved player folder + added wall + wall X resizing

---
+++ Assets/Scene/Main/Wall/Wall.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Wall : MonoBehaviour
+{
+    private void Awake()
+    {
+        var positionXLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        var positionXRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        transform.localScale = new Vector3(
+            (positionXLeft - positionXRight) * 1.25f,
+            transform.localScale.y,
+            transform.localScale.z
+        );
+    }
+}
