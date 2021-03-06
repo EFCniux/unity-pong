5117c76cb6e334eda4b7875c66023c9c50eeb177
Eduardo Ferreras Carracedo <efc.niux@gmail.com>
2021-03-06 21:17:31 +0100

    trigger detection

---
+++ Assets/Scene/Main/Goal/Goal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private bool isRight;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == target)
+        {
+            print(collision.ToString());
+        }
+    }
+}
